189dce7b2cc3a9af03e89c8d7a87e5e2307c5e9f
Daniel Vainerman <danielvain16@gmail.com>
2022-02-10 11:26:12 +0200

    finished homework

--- src/app/services/servers-data.service.ts
+++ src/app/services/servers-data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import ServerData from '../model/ServerData.model';
 
 @Injectable({
@@ -6,7 +7,9 @@ import ServerData from '../model/ServerData.model';
 })
 export class ServersDataService {
   serversArr: ServerData[];
+  serversDataChanged: Subject<ServerData[]>;
   constructor() {
+    this.serversDataChanged = new Subject<ServerData[]>();
     this.serversArr = [
       {
         name: 'Amazon server 1',
@@ -35,13 +38,11 @@ export class ServersDataService {
     ];
   }
   updateServerStatus(serverCode: number, status: boolean): void {
-    if (serverCode < 0 || serverCode > this.serversArr.length - 1) {
-      return;
-    }
     let selectedServer = this.serversArr.find(
       (item) => item.code == serverCode
     );
     selectedServer.status = status;
+    this.serversDataChanged.next(this.serversArr);
     // this.serversArr[serverId].status = status;
   }
 }
